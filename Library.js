91b8a38e004237b9a987fcd793c5d0ba8764a906
zeel.d_v <zeel90dv@gmail.com>
2024-12-29 15:09:23 +0530

    Implement add book feature

---
+++ Library.js
@@ -0,0 +1,32 @@
+class Book {
+    constructor(isbn, title, author, year) {
+      this.isbn = isbn;
+      this.title = title;
+      this.author = author;
+      this.year = year;
+      this.isAvailable = true;
+    }
+  }
+  
+  class Library {
+    constructor() {
+      this.books = [];
+    }
+  
+    addBook(isbn, title, author, year) {
+      try {
+        if (this.books.some(book => book.isbn === isbn)) {
+          throw new Error("Book with this ISBN already exists.");
+        }
+  
+        const book = new Book(isbn, title, author, year);
+        this.books.push(book);
+      } catch (error) {
+        console.error(`Error adding book: ${error.message}`);
+      }
+    }
+  
+  }
+  
+  module.exports = Library;
+  
